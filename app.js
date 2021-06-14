e078fb25d13907609c84052f423bff5e9d92151d
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-06-13 21:21:20 -0500

    use watcher to reset counter when it exceeds 50

--- app.js
+++ app.js
@@ -2,16 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        // solution: refer to in a new constant
+        const that = this;
+        setTimeout(function () {
+          // `this` won't refer to the same value outside this function (aka a JavaScript behavior of `this`)
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' +  this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
   },
   computed: {
     fullname() {
       console.log('Running again...');
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + ' ' + 'Mitchell';
+      return this.name + ' ' + this.lastName;
     }
   },
   methods: {
